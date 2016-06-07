515864db1ae0d416478a929290298599b5931ba8
Jonathan Heilmann <mail@jonathan-heilmann.de>
2016-06-07 16:45:29 +0200

    [RELEASE] Release of version 0.5.0 (closes #1)
    Updated dependencies to support TYPO3 CMS 7 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => false,
 	'createDirs' => '',
 	'clearCacheOnLoad' => false,
-	'version' => '0.4.0',
+	'version' => '0.5.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-7.6.99',
 			'jh_magnificpopup' => '0.4.0',
 		),
 		'conflicts' => array(
